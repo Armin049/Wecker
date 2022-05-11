21d4c902c68d709c7289ba8bd85c3e2ee4fd9bf0
Armin Weber <armin.weber@adesso.de>
2022-05-11 15:10:43 +0200

    added notification

--- app/src/main/java/com/example/wecker/MainActivity.java
+++ app/src/main/java/com/example/wecker/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.wecker;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
 
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
@@ -9,7 +13,13 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.TimePicker;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 public class MainActivity extends AppCompatActivity {
+
+    public static final String CHANNEL_1_ID = "channel1";
+    public String UhrZeit="";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,12 +27,28 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         TimePicker timePicker=findViewById(R.id.timePicker1);
         timePicker.setIs24HourView(true);
+        createNotificationChannels();
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("HH:mm");
+        LocalDateTime now = LocalDateTime.now();
+        UhrZeit=dtf.format(now);
     }
 
     public void setTimer(View view){
         TimePicker timePicker=findViewById(R.id.timePicker1);
         System.out.println(timePicker.getHour());
         System.out.println(timePicker.getMinute());
+
+        NotificationManager notificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+        Intent intent = new Intent(this, empfaengerActivity.class);
+        PendingIntent pIntent = PendingIntent.getActivity(this, (int) System.currentTimeMillis(), intent, 0);
+
+        NotificationCompat.Builder myNoti = new NotificationCompat.Builder(this, CHANNEL_1_ID);
+        myNoti.setAutoCancel(true)
+                .setContentTitle("Wecker; Aufstehen")
+                .setContentText("Es ist "+UhrZeit).setSmallIcon(R.drawable.ic_baseline_access_time_24)
+                .setContentIntent(pIntent);
+
+        notificationManager.notify(0, myNoti.build());
     }
 
     @Override
@@ -43,4 +69,14 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    private void createNotificationChannels() {
+
+        NotificationChannel channel1 = new NotificationChannel(
+                CHANNEL_1_ID,
+                "Channel 1",
+                NotificationManager.IMPORTANCE_HIGH
+        );
+        NotificationManager manager = getSystemService(NotificationManager.class);
+        manager.createNotificationChannel(channel1);
+    }
 }
---
+++ app/src/main/java/com/example/wecker/empfaengerActivity.java
@@ -0,0 +1,14 @@
+package com.example.wecker;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class empfaengerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.alert_activity);
+    }
+}
